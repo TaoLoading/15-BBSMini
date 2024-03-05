34fc7525f2cb93ff8dddf720a5f8f8ec991dae13
taoloading <1377226380@qq.com>
2024-03-05 18:36:42 +0800

    tabBar

--- unpackage/dist/dev/mp-weixin/app.js
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
 const common_vendor = require("./common/vendor.js");
 if (!Math) {
-  "./pages/index/index.js";
   "./pages/home/home.js";
   "./pages/center/center.js";
   "./pages/hot/hot.js";
